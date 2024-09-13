f69c5a0759ecc24fb33c43aaeaa5a7ac4dbee277
angelinaya18 <angelinaya18@mail.ru>
2024-09-13 11:20:13 +0300

    develop-filter

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types"
+import style from "./style.module.css"
+
+export default function Filter({filter, onChange, onClickFind}){
+    return (
+        <>
+            <input className={style.input_search}
+                type="text"
+                value={filter}
+                onChange={(event) => {
+                    onChange(event.target.value);
+                }}
+                id="filter"
+            />
+            <button className={style.button_search} onClick={onClickFind}>Поиск</button>
+        </>
+    )
+}
+
+Filter.propTypes={
+    filter: PropTypes.string.isRequired,
+    onChange: PropTypes.func,
+    onClickFind: PropTypes.func
+}
